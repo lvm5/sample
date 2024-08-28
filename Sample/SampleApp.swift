3e9ab01b33ac5d4b828cc246e9676bb20d2f050a
Leandro Vansan <59117400+lvm5@users.noreply.github.com>
2024-08-27 23:42:06 -0300

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Leandro Morais on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
